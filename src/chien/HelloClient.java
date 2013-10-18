f708f0cc5a6df680bf2c01f26eda70ac3c834b7d
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-10-18 15:07:19 +0200

    Add Client methods

---
+++ src/chien/HelloClient.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chien;
+import chatperche.*;
+import java.net.InetAddress;
+
+/**
+ *
+ * @author Sebastien
+ */
+import java.rmi.*;
+public class HelloClient {
+public static void main(String args[]) {
+    String URL;
+    int port=2069;
+        try {
+            // Récupération d'un stub sur l'objet serveur.
+            // Calcul de l’URL du serveur
+            URL = "//" + InetAddress.getLocalHost().getHostName() + ":"
+                    + port + "/mon_serveur";
+            Chaterface obj = (Chaterface) Naming.lookup(URL);
+            // Appel d'une méthode sur l'objet distant.
+            obj.sayHello();
+        } catch (Exception exc) {
+            System.out.println("Wouaf wouaf !");
+        }
+}
+}
